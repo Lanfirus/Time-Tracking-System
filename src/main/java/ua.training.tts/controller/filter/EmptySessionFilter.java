14152767be984ed833b0873e020c5a0d8df04a4c
Lanfirus <Andrey.dudchenko@gmail.com>
2018-05-07 09:04:13 +0300

    Quick fix to temporary disable EmptySession filter

--- src/main/java/ua.training.tts/controller/filter/EmptySessionFilter.java
+++ src/main/java/ua.training.tts/controller/filter/EmptySessionFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(FilterParameters.URL_PATTERN_ALL)
+//@WebFilter(FilterParameters.URL_PATTERN_ALL)
 public class EmptySessionFilter implements Filter {
 
     @Override
